7c8239eddec10a0d03c6e4f340070062324a567a
Ali Taladar <taladarali@gmail.com>
2025-04-19 19:33:04 -0400

    Added send message endpoint

--- backend/controllers/conversationController.js
+++ backend/controllers/conversationController.js
@@ -1,5 +1,6 @@
 // controllers/conversationController.js
 const Conversation = require('../models/Conversation');
+const Message = require('../models/Message')
 
 const createConversation = async (req, res) => {
   const { title } = req.body;
@@ -30,4 +31,37 @@ const getConversations = async (req, res) => {
   }
 };
 
-module.exports = { createConversation, getConversations };
+
+const createMessage = async (req, res) => {
+  const { content } = req.body;
+  const conversationId = req.params.id;
+  const userId = req.user.id;
+
+  if (!content) {
+    return res.status(400).json({ message: 'Content is required' });
+  }
+
+  try {
+    const conversation = await Conversation.findOne({ _id: conversationId, userId });
+    if (!conversation) {
+      return res.status(404).json({ message: 'Conversation not found' });
+    }
+
+    const newMessage = new Message({
+      conversationId,
+      parentId: null,
+      type: 'user',
+      content,
+      timestamp: new Date(),
+    });
+
+    await newMessage.save();
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+module.exports = { createConversation, getConversations, createMessage };
+
--- backend/routes/conversationRoutes.js
+++ backend/routes/conversationRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { createConversation } = require('../controllers/conversationController');
-const { getConversations } = require('../controllers/conversationController');
+const { createConversation, getConversations, createMessage } = require('../controllers/conversationController');
 const jwtMiddleware = require('../middleware/auth');
 
 router.post('/', jwtMiddleware, createConversation);
 router.get('/', jwtMiddleware, getConversations);
+router.post('/:id/messages', jwtMiddleware, createMessage);
 
 module.exports = router;
